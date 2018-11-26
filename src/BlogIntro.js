de71ceffa0315ef54682726405c90b041d3d2154
matthova <matthova87@gmail.com>
2018-11-25 20:26:12 -0800

    Switching up content

--- src/BlogIntro.js
+++ src/BlogIntro.js
@@ -2,11 +2,9 @@ import React, { Fragment} from 'react';
 
 export const BlogIntro = () => (
   <Fragment>
-    <h1>bla bla bla</h1>
+    <h1>Title and things and stuff</h1>
     <div>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet, erat sed rhoncus vehicula, massa lectus fringilla eros, sit amet ornare leo justo eget sem. Quisque laoreet scelerisque ultricies. Pellentesque tincidunt, mauris nec ultricies pretium, tortor libero lobortis lectus, in tempor nibh libero eget massa. Nullam eget est elementum, posuere diam vel, hendrerit turpis. Sed sit amet nisl ac ligula dapibus dictum sit amet id purus. Sed varius porttitor ex, nec ornare leo eleifend id. Donec pulvinar ex lacus, vitae eleifend tortor luctus id. Sed ac lobortis lectus. Suspendisse ut quam aliquam, tincidunt nibh at, porttitor enim. Donec vitae enim in risus tincidunt ornare. Praesent nec lobortis eros. In arcu turpis, gravida id elementum at, venenatis feugiat lacus.
-
-Integer tempor non elit eget malesuada. Nulla eu nisl purus. Vestibulum a quam eros. Suspendisse posuere ligula et velit porttitor pellentesque. Nullam facilisis, quam id facilisis efficitur, lectus ligula egestas nibh, at venenatis diam sapien sed ex. Morbi tempus commodo sem ac sollicitudin. Integer euismod vitae tellus auctor suscipit. In iaculis sapien urna, et hendrerit eros efficitur quis. Praesent congue nisl in neque ornare ullamcorper. Nam consectetur tortor nunc, non mollis dolor ullamcorper non. Ut non convallis quam, id ultricies nisl. 
+      Mistakes were made. Revalations were had. So on and so forth
     </div>
   </Fragment>
 )
--- src/InstrumentPanel.js
+++ src/InstrumentPanel.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import ReactAudioPlayer from 'react-audio-player';
-import { Box, Flex } from 'rebass';
+import { Box, Flex, Image } from 'rebass';
 
 import * as audioData from './audioData';
 import { AudioChart } from './AudioChart';
@@ -8,66 +8,67 @@ import { AudioChart } from './AudioChart';
 const violinMapping = {
   Amazon_Violin: {
     color: '#654321',
-    description: 'ooooooo',
+    description: 'A longer description will go here talking about details about the print, about how it was different from the last etc etc. a lot of great deatils going to go here for sure. yup, any moment now. going to write some real content here.',
   },
   Hova_Labs_Violin: {
     color: '#654321',
-    description: 'ooooooo',
+    description: 'A longer description will go here talking about details about the print, about how it was different from the last etc etc. a lot of great deatils going to go here for sure. yup, any moment now. going to write some real content here.',
   },
   Hovalin_Solid_Bridge: {
     color: '#000000',
-    description: 'ooooooo',
+    description: 'A longer description will go here talking about details about the print, about how it was different from the last etc etc. a lot of great deatils going to go here for sure. yup, any moment now. going to write some real content here.',
   },
   Hovalin_Solid_Bridge_Matte_Fiber: {
     color: '#000000',
-    description: 'ooooooo',
+    description: 'A longer description will go here talking about details about the print, about how it was different from the last etc etc. a lot of great deatils going to go here for sure. yup, any moment now. going to write some real content here.',
   },
   Hovalin_Wood_Bridge: {
     color: '#FF8000',
-    description: 'ooooooo',
+    description: 'A longer description will go here talking about details about the print, about how it was different from the last etc etc. a lot of great deatils going to go here for sure. yup, any moment now. going to write some real content here.',
   },
   Hovalin_Wood_Bridge_Matte_Fiber: {
     color: '#AAAAAA',
-    description: 'ooooooo',
+    description: 'A longer description will go here talking about details about the print, about how it was different from the last etc etc. a lot of great deatils going to go here for sure. yup, any moment now. going to write some real content here.',
   },
   Hovalin_Wood_Bridge_Four_Perimeters: {
     color: '#D8D800',
-    description: 'ooooooo',
+    description: 'A longer description will go here talking about details about the print, about how it was different from the last etc etc. a lot of great deatils going to go here for sure. yup, any moment now. going to write some real content here.',
   },
   Hovalin_Wood_Bridge_Seven_Perimeters: {
     color: '#00FF00',
-    description: 'ooooooo',
+    description: 'A longer description will go here talking about details about the print, about how it was different from the last etc etc. a lot of great deatils going to go here for sure. yup, any moment now. going to write some real content here.',
   },
   Hovalin_Wood_Bridge_Sideways: {
     color: '#FF0000',
-    description: 'ooooooo',
+    description: 'A longer description will go here talking about details about the print, about how it was different from the last etc etc. a lot of great deatils going to go here for sure. yup, any moment now. going to write some real content here.',
   },
   Modular_Fiddle: {
     color: '#008080',
-    description: 'ooooooo',
+    description: 'A longer description will go here talking about details about the print, about how it was different from the last etc etc. a lot of great deatils going to go here for sure. yup, any moment now. going to write some real content here.',
   },
 };
 
 export const InstrumentPanel = ({ name }) => {
   const { color, description } = violinMapping[name];
   const data = audioData[name];
+  const nameWithSpaces = name.replace(/_/g, ' ');
 
   return (
-    <Flex flexDirection="row">
-      <Box flex="0 1 200px">
-        <Box>{name.replace(/_/g, ' ')}</Box>
+    <Flex p="20px" flexDirection="row">
+      <Box flex="1" >
+        <Box>{nameWithSpaces}</Box>
         <ReactAudioPlayer
           src={`sampleFiles/${name}.mp3`}
           controls
         />
         <Box>{description}</Box>
       </Box>
-      <Flex flex="1" flexDirection="row" alignContent="flexEnd" width="100%">
-        <Box flex="1" width="300px">
-          <img alt={name} src={`./violinPhotos/${name}.jpg`} style={{width: 'inherit'}}/>
-        </Box>
+      <Flex flex="1" flexDirection="column" alignItems="center" width="100%">
+        <Flex flex="1" width="300px" alignItems="center">
+          <Image src={`./violinPhotos/${name}.jpg`} />
+        </Flex>
         <Box flex="1" width="100%">
-          <AudioChart title={name} datasets={[{ color, data, label: name }]}/>
+          <AudioChart title={nameWithSpaces + ": Audio Frequency Response"} datasets={[{ color, data, label: nameWithSpaces }]}/>
         </Box>
       </Flex>
     </Flex>
